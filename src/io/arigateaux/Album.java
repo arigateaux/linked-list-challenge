2a2c09bc1fe2a17254a8e6a03755ab42e0799763
Arigateaux <dvanfoss@gmail.com>
2018-02-09 21:53:30 -0500

    ..

--- src/io/arigateaux/Album.java
+++ src/io/arigateaux/Album.java
@@ -1,4 +1,45 @@
 package io.arigateaux;
 
+import java.util.LinkedList;
+
 public class Album {
+
+    private LinkedList<Song> songList;
+    private String albumName;
+    private String songTitle;
+    private int duration;
+
+    public Album(String albumName) {
+        songList = new LinkedList<Song>();
+        this.albumName = albumName;
+    }
+
+    public boolean addSongToAlbum(String albumName, String songTitle, int duration) {
+        if (findSong(albumName, songTitle) != null) {
+            songList.add(new Song(songTitle, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String albumName, String songTitle) {
+        for (int i = 0; i <= songList.size(); i++) {
+            Song currentSong = songList.get(i);
+            if (currentSong.getTitle().equals(songTitle))
+                return currentSong;
+        }
+        return null;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
--- src/io/arigateaux/Main.java
+++ src/io/arigateaux/Main.java
@@ -1,8 +1,84 @@
 package io.arigateaux;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+    public static Scanner scanner;
+    public static ArrayList<Album> playlist;
+    public static boolean isRunning = false;
+
     public static void main(String[] args) {
-	// write your code here
+
+        playlist = new ArrayList<Album>();
+        scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        init();
+        while (isRunning) {
+            switch (input) {
+                // exit
+                case 0:
+                    isRunning = false;
+                    break;
+                // forward
+                case 1:
+                    nextSong();
+                    break;
+                // backward
+                case 2:
+                    previousSong();
+                    break;
+                // replay
+                case 3:
+                    replaySong();
+                    break;
+                // list
+                case 4:
+                    listSongs();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+
+    public static void init() {
+        System.out.println(
+                "Please choose an option:\n" +
+                "0 - Quit\n" +
+                "1 - Next Song\n" +
+                "2 - Previous Song\n" +
+                "3 - Replay Song\n" +
+                "4 - List Songs\n");
+    }
+
+    public static void addAlbum(String albumName) {
+        Album album = new Album(albumName);
+        if (!playlist.contains(album)) {
+            playlist.add(album);
+        }
+    }
+
+    public static void addSong(String albumName, String songName, int duration) {
+
+    }
+
+    public static void nextSong() {
+
+    }
+
+    public static void previousSong() {
+
+    }
+
+    public static void replaySong() {
+
+    }
+
+    public static void listSongs() {
+
+    }
+
 }
